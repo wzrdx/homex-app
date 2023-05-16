dfb7ea0eecd63ad82299379feeba29d1e58dd09d
wzrdx <128477299+wzrdx@users.noreply.github.com>
2023-05-16 17:58:43 +0300

    Leaderboard add tickets

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,9 +1,10 @@
-import { Box, Flex, Spinner, Text } from '@chakra-ui/react';
+import { Box, Flex, Spinner, Text, Image } from '@chakra-ui/react';
 import { useEffect } from 'react';
 import { TicketEarner, useGetTicketEarners } from '../blockchain/hooks/useGetTicketEarners';
 import { map } from 'lodash';
 import { getShortAddress } from '../services/helpers';
 import { Role, RoleTag } from '../shared/RoleTag';
+import { RESOURCE_ELEMENTS } from '../services/resources';
 
 function Leaderboard() {
     const { earners, getTicketEarners } = useGetTicketEarners();
@@ -32,8 +33,18 @@ function Leaderboard() {
                     <Flex flexDir="column">
                         {map(earners, (earner: TicketEarner, index: number) => (
                             <Flex mt={2} alignItems="center" key={index}>
-                                <Text>{getShortAddress(earner.address)}</Text>
-                                <Text mx={6}>{earner.ticketsEarned}</Text>
+                                <Text minWidth="100px" textAlign="left">
+                                    {getShortAddress(earner.address)}
+                                </Text>
+
+                                <Text minWidth="24px" textAlign="right">
+                                    {earner.ticketsEarned}
+                                </Text>
+
+                                <Flex mr={4} minWidth="32px" ml={1}>
+                                    <Image height="28px" src={RESOURCE_ELEMENTS['tickets'].icon} />
+                                </Flex>
+
                                 {getRoleTag(earner.ticketsEarned)}
                             </Flex>
                         ))}
