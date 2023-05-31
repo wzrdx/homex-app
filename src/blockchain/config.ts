f14d853bb54584fe0170b4263ae4cacb08958f53
Serban Macrineanu <s.macrineanu@gmail.com>
2023-06-01 01:09:47 +0300

    Final values

--- src/blockchain/config.ts
+++ src/blockchain/config.ts
@@ -1,4 +1,4 @@
-export const contractAddress = 'erd1qqqqqqqqqqqqqpgq35v9rqput4tpgzzmernvffvvhdgcex3aukrqmpwthn';
+export const contractAddress = 'erd1qqqqqqqqqqqqqpgqj45qe9v5y8sk9gzhej9tydd8s6puucc4ukrq44n0aj';
 
 export const walletConnectV2ProjectId = '323a2ff473ee6b636d89a1a83ec733e2';
 
@@ -9,11 +9,11 @@ export const TOKEN_DENOMINATION = 1000000;
 export const API_URL = 'https://elrond-api-devnet.blastapi.io/10364add-1eae-4391-acd6-5f7ac77ad457';
 export const GATEWAY_URL = 'https://gateway.multiversx.com';
 
-export const TICKETS_TOKEN_ID = 'HOMETICKET-c81e1c';
-export const ENERGY_TOKEN_ID = 'ENERGY-d91318';
-export const HERBS_TOKEN_ID = 'HERBS-0ca66e';
-export const GEMS_TOKEN_ID = 'GEMS-786101';
-export const ESSENCE_TOKEN_ID = 'ESSENCE-59ca4b';
+export const TICKETS_TOKEN_ID = 'HOMETICKET-de72b4';
+export const ENERGY_TOKEN_ID = 'ENERGY-834e7b';
+export const HERBS_TOKEN_ID = 'HERBS-1513b3';
+export const GEMS_TOKEN_ID = 'GEMS-40a073';
+export const ESSENCE_TOKEN_ID = 'ESSENCE-deb37b';
 
 export const START_OF_CONTEST = new Date('2023-06-01T06:00:00.000Z');
 export const END_OF_CONTEST = new Date('2023-06-02T06:00:00.000Z');
