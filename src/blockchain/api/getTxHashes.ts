538f62b54608b728e80aded3d356693748b93acf
wzrdx <128477299+wzrdx@users.noreply.github.com>
2023-06-09 18:03:45 +0300

    Prizes w/ tx hashes final implementation

---
+++ src/blockchain/api/getTxHashes.ts
@@ -0,0 +1,29 @@
+import { ResultsParser, ContractFunction } from '@multiversx/sdk-core/out';
+import { ProxyNetworkProvider } from '@multiversx/sdk-network-providers/out';
+import { smartContract } from '../smartContract';
+import { API_URL } from '../config';
+import { map } from 'lodash';
+
+const resultsParser = new ResultsParser();
+const proxy = new ProxyNetworkProvider(API_URL);
+const FUNCTION_NAME = 'getTxHashes';
+
+export const getTxHashes = async () => {
+    try {
+        const query = smartContract.createQuery({
+            func: new ContractFunction(FUNCTION_NAME),
+        });
+
+        const queryResponse = await proxy.queryContract(query);
+        const endpointDefinition = smartContract.getEndpoint(FUNCTION_NAME);
+
+        const { firstValue } = resultsParser.parseQueryResponse(queryResponse, endpointDefinition);
+        const array = firstValue?.valueOf();
+
+        const parsedArray = map(array, (hash) => Buffer.from(hash).toString('hex'));
+        return parsedArray;
+    } catch (err) {
+        console.error(`Unable to call ${FUNCTION_NAME}`, err);
+        return [];
+    }
+};
--- src/components/Rewards/Prizes.tsx
+++ src/components/Rewards/Prizes.tsx
@@ -1,29 +1,65 @@
 import _ from 'lodash';
 import { Box, Flex, Spinner, Text, Image, Link } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
-import { getShortAddress, getTxExplorerURL, getUsername } from '../../services/helpers';
+import { getShortAddress, getTxExplorerURL, getUsername, sleep } from '../../services/helpers';
 import { API_URL, EGLD_DENOMINATION } from '../../blockchain/config';
 import axios from 'axios';
-import { ExternalLinkIcon } from '@chakra-ui/icons';
-
-const TX_HASH = 'b6a01cb84ce09a01f188f07cf72727e627bfa699467412e1458433391cac99f5';
+import { ExternalLinkIcon, CalendarIcon, InfoOutlineIcon } from '@chakra-ui/icons';
+import { getTxHashes } from '../../blockchain/api/getTxHashes';
+import { format } from 'date-fns';
+import { useRewards } from '../Rewards';
+import { Timer } from '../../shared/Timer';
+import { getRaffleTimestamp } from '../../blockchain/api/getRaffleTimestamp';
 
 function Prizes() {
-    const [raffle, setRaffle] = useState<{
+    const { height } = useRewards();
+    const [timestamp, setTimestamp] = useState<Date>();
+
+    const [tx, setTx] = useState<{
         winners: Array<{
             username: string;
             prize: string;
         }>;
         timestamp: Date;
+        hash: string;
     }>();
 
-    // Init
+    const [hashes, setHashes] = useState<string[]>([]);
+    const [isLoadingHashes, setLoadingHashes] = useState<boolean>(true);
+
     useEffect(() => {
-        getRaffleTx();
+        init();
     }, []);
 
-    const getRaffleTx = async () => {
-        const result = await axios.get(`transactions/${TX_HASH}`, {
+    useEffect(() => {
+        if (hashes.length) {
+            getTx(_.head(hashes) as string);
+        }
+    }, [hashes]);
+
+    const init = async () => {
+        try {
+            setTimestamp(await getRaffleTimestamp());
+
+            const hashes = await getTxHashes();
+
+            setHashes(hashes);
+
+            if (hashes.length) {
+                await getTx(_.head(hashes) as string);
+            }
+
+            setLoadingHashes(false);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const getTx = async (hash: string) => {
+        // Used to trigger the spinner
+        setTx(undefined);
+
+        const result = await axios.get(`transactions/${hash}`, {
             baseURL: API_URL,
             params: {
                 fields: 'operations,timestamp',
@@ -40,43 +76,92 @@ function Prizes() {
 
             const timestamp = new Date(result.data.timestamp * 1000);
 
-            console.log({
+            setTx({
                 winners,
                 timestamp,
-            });
-
-            setRaffle({
-                winners,
-                timestamp,
+                hash,
             });
         }
     };
 
     return (
-        <Flex justifyContent="center" height="100%">
-            {!raffle ? (
+        <Flex height={_.isEmpty(hashes) ? 'auto' : `calc(100% - ${height}px)`} justifyContent="center">
+            {isLoadingHashes ? (
                 <Spinner />
+            ) : _.isEmpty(hashes) ? (
+                <Flex flexDir="column" alignItems="Center">
+                    <Text mb={3}>Prizes distribution will be displayed here after the raffle ends</Text>
+
+                    <Flex minWidth="158px">
+                        <Timer timestamp={timestamp as Date} isActive isDescending displayDays />
+                    </Flex>
+                </Flex>
             ) : (
-                <Flex flexDir="column" alignItems="center">
-                    <Link href={getTxExplorerURL(TX_HASH)} isExternal>
-                        <Flex alignItems="center">
-                            <Text>{getShortAddress(TX_HASH)}</Text>
-                            <ExternalLinkIcon ml={1.5} />
-                        </Flex>
-                    </Link>
+                <Flex minW="448px">
+                    {/* Left */}
+                    <Flex flex={1} flexDir="column" overflowY="auto" pr={4}>
+                        {_.map(hashes, (hash, index) => (
+                            <Text
+                                key={index}
+                                color={tx?.hash === hash ? 'white' : 'header.gray'}
+                                _notLast={{ mb: 2 }}
+                                cursor="pointer"
+                                transition="all 0.4s cubic-bezier(0.215, 0.610, 0.355, 1)"
+                                _hover={{ color: '#e3e3e3' }}
+                                onClick={() => getTx(hash)}
+                            >
+                                Trial #{index + 1}
+                            </Text>
+                        ))}
+                    </Flex>
 
-                    <Text mt={4}>{raffle.timestamp.toString()}</Text>
-                    <Text my="4" fontSize="17px" fontWeight={500}>
-                        Winners
-                    </Text>
+                    {/* Right */}
+                    <Flex flex={4} flexDir="column" px={10} overflowY="auto">
+                        {!tx ? (
+                            <Flex justifyContent="center">
+                                <Spinner />
+                            </Flex>
+                        ) : (
+                            <>
+                                <Flex alignItems="center" mb={2}>
+                                    <CalendarIcon mr={2} fontSize="14px" color="whiteAlpha.900" />
+                                    <Text>{format(tx.timestamp, 'PPPP')}</Text>
+                                </Flex>
 
-                    {_.map(raffle.winners, (winner, index) => (
-                        <Flex width="38%" justifyContent="space-between" key={index}>
-                            <Text mr="2">{winner.username}</Text>
+                                <Link href={getTxExplorerURL(tx.hash)} isExternal>
+                                    <Flex alignItems="center">
+                                        <Text>{getShortAddress(tx.hash)}</Text>
+                                        <ExternalLinkIcon ml={1.5} />
+                                    </Flex>
+                                </Link>
 
-                            <Text>{Number.parseInt(winner.prize) / EGLD_DENOMINATION} $EGLD</Text>
-                        </Flex>
-                    ))}
+                                <Text mt={6} mb={2} fontSize="17px" fontWeight={500}>
+                                    Winners
+                                </Text>
+
+                                <Flex width="100%" flexDir="column">
+                                    {_.map(tx.winners, (winner, index) => (
+                                        <Flex
+                                            key={index}
+                                            width="100%"
+                                            justifyContent="space-between"
+                                            alignItems="center"
+                                            _notLast={{ mb: 1 }}
+                                        >
+                                            <Text mr="2">{winner.username}</Text>
+
+                                            <Text>
+                                                <Text as="span" color="brightBlue">
+                                                    {Number.parseInt(winner.prize) / EGLD_DENOMINATION}
+                                                </Text>{' '}
+                                                $EGLD
+                                            </Text>
+                                        </Flex>
+                                    ))}
+                                </Flex>
+                            </>
+                        )}
+                    </Flex>
                 </Flex>
             )}
         </Flex>
