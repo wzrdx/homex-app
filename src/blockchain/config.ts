9f44037c9a9e6d725c1798513a078ebaa7b013c1
wzrdx <128477299+wzrdx@users.noreply.github.com>
2023-06-13 16:55:44 +0300

    Mainnet clean values

--- src/blockchain/config.ts
+++ src/blockchain/config.ts
@@ -1,5 +1,5 @@
 // TODO: Mainnet values
-export const contractAddress = 'erd1qqqqqqqqqqqqqpgq9459wl67kadq47jq8xqwp56muskyaajvukrq9xjh8z';
+export const contractAddress = '';
 
 export const walletConnectV2ProjectId = '323a2ff473ee6b636d89a1a83ec733e2';
 
@@ -7,27 +7,24 @@ export const apiTimeout = 30000;
 export const EGLD_DENOMINATION = 1000000000000000000;
 export const TOKEN_DENOMINATION = 1000000;
 
-// TODO: Mainnet values
-export const CHAIN_ID = 'D';
-export const API_URL = 'https://devnet-api.multiversx.com';
-// export const DEVNET_API_URL = 'https://elrond-api-devnet.blastapi.io/10364add-1eae-4391-acd6-5f7ac77ad457';
-// export const API_URL = 'https://elrond-api.blastapi.io/66bcc344-d7d2-4bdf-8f41-a0ee9d91318b'
+export const CHAIN_ID = '1';
+export const API_URL = 'https://elrond-api.blastapi.io/66bcc344-d7d2-4bdf-8f41-a0ee9d91318b';
 export const GATEWAY_URL = 'https://gateway.multiversx.com';
-export const EXPLORER_URL = 'https://devnet-explorer.multiversx.com';
+export const EXPLORER_URL = 'https://explorer.multiversx.com';
 
 // TODO: Mainnet values
-export const TRAVELERS_COLLECTION_ID = 'HOLYCOWS-90e467';
-export const ELDERS_COLLECTION_ID = 'PINKF-f70e86';
-export const TICKETS_TOKEN_ID = 'TICKETS-be6092';
-export const ENERGY_TOKEN_ID = 'ENERGY-a1ac9c';
-export const HERBS_TOKEN_ID = 'HERBS-a2ffd9';
-export const GEMS_TOKEN_ID = 'GEMS-274ae8';
-export const ESSENCE_TOKEN_ID = 'ESSENCE-7ae5f2';
+export const TRAVELERS_COLLECTION_ID = '';
+export const ELDERS_COLLECTION_ID = '';
+export const TICKETS_TOKEN_ID = '';
+
+export const ENERGY_TOKEN_ID = '';
+export const HERBS_TOKEN_ID = '';
+export const GEMS_TOKEN_ID = '';
+export const ESSENCE_TOKEN_ID = '';
 
 export const TRAVELER_YIELD_PER_HOUR: number = 3;
 export const ELDER_YIELD_PER_HOUR: number = 3;
 export const REWARDS_QUERYING_INTERVAL = 30000;
 export const ENERGY_SWAP_RATE = 0.000024;
 
-// TODO: Mainnet values
-export const START_OF_CONTEST = new Date('2023-06-01T06:00:00.000Z');
+export const START_OF_CONTEST = new Date('2023-06-13T16:00:00.000Z');
