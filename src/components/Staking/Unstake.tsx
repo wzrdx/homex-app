f584a25a9b3bac7725f23529f879dfff8eb9236d
wzrdx <128477299+wzrdx@users.noreply.github.com>
2023-07-04 23:19:58 +0300

    Unstake 20 mil. gas limit

--- src/components/Staking/Unstake.tsx
+++ src/components/Staking/Unstake.tsx
@@ -181,7 +181,7 @@ function Unstake() {
                 .unstake([travelerNonces, elderNonces])
                 .withSender(user)
                 .withChainID(CHAIN_ID)
-                .withGasLimit(12000000 + 1000000 * count)
+                .withGasLimit(20000000 + 1000000 * count)
                 .buildTransaction();
 
             await refreshAccount();
@@ -226,7 +226,7 @@ function Unstake() {
                 .claimStakingRewards()
                 .withSender(user)
                 .withChainID(CHAIN_ID)
-                .withGasLimit(11000000)
+                .withGasLimit(20000000)
                 .buildTransaction();
 
             await refreshAccount();
