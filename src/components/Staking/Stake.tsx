56eed73c18cf399a915bcfbde3b834eedf151e59
wzrdx <128477299+wzrdx@users.noreply.github.com>
2023-09-03 21:30:24 +0300

    Stake gasLimit

--- src/components/Staking/Stake.tsx
+++ src/components/Staking/Stake.tsx
@@ -78,7 +78,7 @@ function Stake() {
                 .withSender(user)
                 .withExplicitReceiver(user)
                 .withChainID(CHAIN_ID)
-                .withGasLimit(11000000 + 130000 * stakedNFTsCount + 1600000 * _.size(transfers))
+                .withGasLimit(14000000 + 150000 * stakedNFTsCount + 1600000 * _.size(transfers))
                 .buildTransaction();
 
             await refreshAccount();
