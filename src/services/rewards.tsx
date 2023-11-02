3f72feebe044409040a8c3673f9f7191a879c4fb
wzrdx <128477299+wzrdx@users.noreply.github.com>
2023-11-02 14:35:07 +0200

    Trial 13 Rewards

--- src/services/rewards.tsx
+++ src/services/rewards.tsx
@@ -743,6 +743,35 @@ export const RAFFLES: any[] = [
         nonce: 614,
         url: 'https://media.elrond.com/nfts/asset/bafybeidixut3brb7brnjow42l2mu7fbw7dbkghbpsavbhaewcbeeum7mpi/2898.png',
     },
+    // Trial 13 - Starts with ID 92
+    {
+        type: RewardType.NFT,
+        id: getId(),
+        winners: 1,
+        name: 'GenesisSpaceApe #12573',
+        nonce: 13790,
+        url: 'https://media.elrond.com/nfts/asset/bafybeicsa2ygmod3btvjtdvf6pofhp4vtjgfzgpmjhmgiryunrceccxmee/12573.png',
+    },
+    {
+        type: RewardType.NFT,
+        id: getId(),
+        winners: 1,
+        name: 'GenesisSpaceApe #9994',
+        nonce: 3863,
+        url: 'https://media.elrond.com/nfts/asset/bafybeicsa2ygmod3btvjtdvf6pofhp4vtjgfzgpmjhmgiryunrceccxmee/9994.png',
+    },
+    {
+        type: RewardType.Prizes,
+        id: getId(),
+        prizes: [getEGLDPrize(0.75)],
+        winners: 1,
+    },
+    {
+        type: RewardType.Prizes,
+        id: getId(),
+        prizes: [getEGLDPrize(0.75)],
+        winners: 1,
+    },
 ];
 
 export const BATTLES: any[] = [
