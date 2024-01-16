5d70428bbcdb42cac00fcd6c6159ad3c6796f0bb
wzrdx <128477299+wzrdx@users.noreply.github.com>
2024-01-16 19:10:38 +0200

    Staking gasLimit

--- src/components/Staking/Unstake.tsx
+++ src/components/Staking/Unstake.tsx
@@ -259,7 +259,7 @@ function Unstake() {
                 .claimStakingRewards()
                 .withSender(user)
                 .withChainID(CHAIN_ID)
-                .withGasLimit(20000000 + 500000 * stakedNFTsCount)
+                .withGasLimit(28000000 + 500000 * stakedNFTsCount)
                 .buildTransaction();
 
             await refreshAccount();
