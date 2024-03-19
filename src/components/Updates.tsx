4a788aa0d483250ec09a50d6f9c0a7ee65a6511b
wzrdx <128477299+wzrdx@users.noreply.github.com>
2024-03-19 13:06:50 +0200

    Updates modal

--- src/components/Updates.tsx
+++ src/components/Updates.tsx
@@ -1,21 +1,22 @@
 import _ from 'lodash';
 import { Text, Image, Stack } from '@chakra-ui/react';
-import { getRareTravelersRareAssets, getRareTravelersRoyalAssets } from '../services/assets';
+import { getSummaryAssets } from '../services/assets';
 
 export const Updates = ({}) => {
     return (
         <Stack justifyContent="center" spacing={6} alignItems="center">
             <Stack direction="row" spacing={8}>
-                <Image src={_.last(getRareTravelersRareAssets(3))} maxW="180px" />
-                <Image src={_.last(getRareTravelersRoyalAssets(3))} maxW="180px" />
+                <Image src={_.last(getSummaryAssets(0, 'Occult'))} maxW="142px" />
+                <Image src={_.last(getSummaryAssets(4, 'Esoteric'))} maxW="142px" />
+                <Image src={_.last(getSummaryAssets(5, 'Magical'))} maxW="142px" />
             </Stack>
 
-            <Text fontWeight={500} fontSize="17px" letterSpacing="0.5px" textAlign="center" maxW="382px">
-                The{' '}
+            <Text fontWeight={500} fontSize="17px" letterSpacing="0.5px" textAlign="center">
+                Three new{' '}
                 <Text as="span" color="page" fontWeight={500}>
-                    Rare Travelers
+                    pages
                 </Text>{' '}
-                page has been added to the Traveler's Log.
+                have been added to the Traveler's Log.
             </Text>
         </Stack>
     );
