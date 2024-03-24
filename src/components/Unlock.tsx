f42bd1f3fff03ccf78bad643d07a70422c7a0d42
wzrdx <128477299+wzrdx@users.noreply.github.com>
2024-03-24 20:55:06 +0200

    Unlock 1

--- src/components/Unlock.tsx
+++ src/components/Unlock.tsx
@@ -4,21 +4,22 @@ import {
     LedgerLoginButton,
     WalletConnectLoginButton,
 } from '@multiversx/sdk-dapp/UI';
-import { Box, Text, Spinner, Flex, Stack, Image } from '@chakra-ui/react';
+import { Box, Text, Spinner, Flex, Stack, Image, Center, Link } from '@chakra-ui/react';
 import { useGetAccountInfo } from '@multiversx/sdk-dapp/hooks';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthenticationContextType, getNFTsCount, useAuthenticationContext } from '../services/authentication';
 import { logout } from '@multiversx/sdk-dapp/utils';
-import { ChevronRightIcon } from '@chakra-ui/icons';
+import { ArrowForwardIcon, ChevronRightIcon } from '@chakra-ui/icons';
 import { MdExtension, MdWeb } from 'react-icons/md';
 import Ledger from '../assets/icons/Ledger.png';
 import MultiversX from '../assets/icons/MultiversX.png';
 import { ELDERS_COLLECTION_ID, TEAM, TRAVELERS_COLLECTION_ID, walletConnectV2ProjectId } from '../blockchain/config';
 import Wallet from '../shared/Wallet';
-import { getUnlockBackground } from '../services/assets';
-import { getBackgroundStyle } from '../services/helpers';
+import { getBackground } from '../services/assets';
 import { useStoreContext, StoreContextType } from '../services/store';
+import MazeMask from '../assets/images/maze_mask.png';
+import LogoMirage from '../assets/logo_mirage.png';
 
 enum AuthenticationError {
     NotHolder = 'NotHolder',
@@ -62,8 +63,6 @@ const Unlock = () => {
                 const { data: travelerTokens } = await getNFTsCount(address, TRAVELERS_COLLECTION_ID);
                 const { data: elderTokens } = await getNFTsCount(address, ELDERS_COLLECTION_ID);
 
-                console.warn('Holder', travelerTokens + elderTokens);
-
                 if (travelerTokens + elderTokens === 0) {
                     setError(AuthenticationError.NotHolder);
                     return;
@@ -75,133 +74,153 @@ const Unlock = () => {
         }
     };
 
-    const getText = (text: string, displayTimer = false) => (
-        <Flex flexDir="column" alignItems="center">
-            <Text fontSize="17px" fontWeight={500} color="white" textAlign="center">
-                {text}
+    const getError = () => (
+        <Stack spacing={8} alignItems="center">
+            <Text fontSize="17px" fontWeight={500} textAlign="center">
+                Playing the game requires an NFT from the following{' '}
+                <Text as="span" color="redClrs">
+                    Home X
+                </Text>{' '}
+                collections:
             </Text>
 
-            <Box mt={5}>
-                <Wallet
-                    callback={() => {
-                        setError(undefined);
-                    }}
-                />
-            </Box>
-        </Flex>
+            <Stack spacing={1}>
+                <Link href="https://xoxno.com/collection/TRAVELER-51bdef" isExternal _hover={{ opacity: 0.75 }}>
+                    <Stack direction="row" spacing={1} alignItems="center">
+                        <ArrowForwardIcon fontSize="18px" />
+                        <Text fontWeight={500}>The First Travelers</Text>
+                    </Stack>
+                </Link>
+
+                <Link href="https://xoxno.com/collection/HOMEXELDER-d43957" isExternal _hover={{ opacity: 0.75 }}>
+                    <Stack direction="row" spacing={1} alignItems="center">
+                        <ArrowForwardIcon fontSize="18px" />
+                        <Text fontWeight={500}>The Elders</Text>
+                    </Stack>
+                </Link>
+            </Stack>
+
+            <Wallet
+                callback={() => {
+                    setError(undefined);
+                }}
+            />
+        </Stack>
     );
 
     return (
         <Flex
             backgroundColor="dark"
-            style={getBackgroundStyle(getUnlockBackground())}
+            backgroundImage={`url(${getBackground()})`}
+            backgroundSize="cover"
+            backgroundPosition="center"
+            backgroundRepeat="no-repeat"
             height="100vh"
             width="100vw"
             justifyContent="center"
             alignItems="center"
             color="whitesmoke"
         >
-            <Box
-                position="relative"
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                flexDirection="column"
-                width="394px"
-                borderRadius="md"
-                overflow="hidden"
-                py="6"
-                px="8"
-            >
+            <Center position="relative" flexDirection="column" py="6" px="8">
                 {!error && !isUserLoggedIn() && (
-                    <Text
-                        textTransform="uppercase"
-                        fontSize="22px"
-                        fontWeight="500"
-                        letterSpacing="0.5px"
-                        mb="1.5rem"
-                        color="whitesmoke"
-                        textShadow="0 0 4px rgb(0 0 0 / 30%)"
-                    >
-                        Connect wallet
-                    </Text>
+                    <Stack spacing={3.5} mb={5}>
+                        <Image src={LogoMirage} maxW="94px" margin="0 auto" />
+
+                        <Text
+                            layerStyle="faustina"
+                            textTransform="uppercase"
+                            fontSize="29px"
+                            fontWeight="600"
+                            letterSpacing="1px"
+                            textShadow="0 0 4px rgb(0 0 0 / 30%)"
+                            textAlign="center"
+                        >
+                            Season{' '}
+                            <Text as="span" color="mirage" fontWeight={800}>
+                                2
+                            </Text>
+                        </Text>
+                    </Stack>
                 )}
 
-                <Stack width="100%">
-                    {!isUserLoggedIn() ? (
-                        <>
-                            <ExtensionLoginButton buttonClassName="Login-Button" {...commonProps}>
-                                <Box display="flex" alignItems="center" justifyContent="space-between">
-                                    <Box display="flex" alignItems="center">
-                                        <Box mb="1px" pl="2px" width="36px">
-                                            <MdExtension fontSize="19px" />
-                                        </Box>
-                                        <Text fontSize="16px">Browser Extension</Text>
+                {!isUserLoggedIn() ? (
+                    <Stack
+                        position="relative"
+                        width="282px"
+                        style={{
+                            maskImage: `url(${MazeMask})`,
+                            maskSize: '282px',
+                            maskRepeat: 'no-repeat',
+                        }}
+                    >
+                        <ExtensionLoginButton buttonClassName="Login-Button" {...commonProps}>
+                            <Box display="flex" alignItems="center" justifyContent="space-between">
+                                <Box display="flex" alignItems="center">
+                                    <Box mb="1px" pl="2px" width="36px">
+                                        <MdExtension fontSize="19px" />
                                     </Box>
-
-                                    <ChevronRightIcon color="#f5f5f5db" boxSize={6} />
+                                    <Text fontSize="17px">Browser Extension</Text>
                                 </Box>
-                            </ExtensionLoginButton>
 
-                            <WebWalletLoginButton buttonClassName="Login-Button" callbackRoute="/" {...commonProps}>
-                                <Box display="flex" alignItems="center" justifyContent="space-between">
-                                    <Box display="flex" alignItems="center">
-                                        <Box width="36px">
-                                            <MdWeb fontSize="21px" />
-                                        </Box>
-                                        <Text fontSize="16px">MultiversX Web Wallet</Text>
+                                <ChevronRightIcon color="#f5f5f5db" boxSize={6} />
+                            </Box>
+                        </ExtensionLoginButton>
+
+                        <WebWalletLoginButton buttonClassName="Login-Button" callbackRoute="/" {...commonProps}>
+                            <Box display="flex" alignItems="center" justifyContent="space-between">
+                                <Box display="flex" alignItems="center">
+                                    <Box width="36px">
+                                        <MdWeb fontSize="21px" />
                                     </Box>
-
-                                    <ChevronRightIcon color="#f5f5f5db" boxSize={6} />
+                                    <Text fontSize="17px">MultiversX Web Wallet</Text>
                                 </Box>
-                            </WebWalletLoginButton>
 
-                            <WalletConnectLoginButton
-                                buttonClassName="Login-Button"
-                                {...commonProps}
-                                {...(walletConnectV2ProjectId
-                                    ? {
-                                          isWalletConnectV2: true,
-                                      }
-                                    : {})}
-                            >
-                                <Box display="flex" alignItems="center" justifyContent="space-between">
-                                    <Box display="flex" alignItems="center">
-                                        <Box width="36px" pl="2px">
-                                            <Image src={MultiversX} alt="MultiversX" width="18px" height="18px" />
-                                        </Box>
-                                        <Text fontSize="16px">xPortal App</Text>
+                                <ChevronRightIcon color="#f5f5f5db" boxSize={6} />
+                            </Box>
+                        </WebWalletLoginButton>
+
+                        <WalletConnectLoginButton
+                            buttonClassName="Login-Button"
+                            {...commonProps}
+                            {...(walletConnectV2ProjectId
+                                ? {
+                                      isWalletConnectV2: true,
+                                  }
+                                : {})}
+                        >
+                            <Box display="flex" alignItems="center" justifyContent="space-between">
+                                <Box display="flex" alignItems="center">
+                                    <Box width="36px" pl="2px">
+                                        <Image src={MultiversX} alt="MultiversX" width="18px" height="18px" />
                                     </Box>
-
-                                    <ChevronRightIcon color="#f5f5f5db" boxSize={6} />
+                                    <Text fontSize="17px">xPortal App</Text>
                                 </Box>
-                            </WalletConnectLoginButton>
 
-                            <LedgerLoginButton buttonClassName="Login-Button" {...commonProps}>
-                                <Box display="flex" alignItems="center" justifyContent="space-between">
-                                    <Box display="flex" alignItems="center">
-                                        <Box width="36px" pl="3px">
-                                            <Image src={Ledger} alt="Ledger" width="16px" height="16px" />
-                                        </Box>
-                                        <Text fontSize="16px">Ledger</Text>
+                                <ChevronRightIcon color="#f5f5f5db" boxSize={6} />
+                            </Box>
+                        </WalletConnectLoginButton>
+
+                        <LedgerLoginButton buttonClassName="Login-Button" {...commonProps}>
+                            <Box display="flex" alignItems="center" justifyContent="space-between">
+                                <Box display="flex" alignItems="center">
+                                    <Box width="36px" pl="3px">
+                                        <Image src={Ledger} alt="Ledger" width="16px" height="16px" />
                                     </Box>
-
-                                    <ChevronRightIcon color="#f5f5f5db" boxSize={6} />
+                                    <Text fontSize="17px">Ledger</Text>
                                 </Box>
-                            </LedgerLoginButton>
-                        </>
-                    ) : (
-                        <Box display="flex" flexDir="column" alignItems="center">
-                            {/* {error === AuthenticationError.ContestNotStarted && getText('Waiting for the game to start', true)} */}
 
-                            {error === AuthenticationError.NotHolder &&
-                                getText('Playing the game requires an NFT from the Home X collections')}
+                                <ChevronRightIcon color="#f5f5f5db" boxSize={6} />
+                            </Box>
+                        </LedgerLoginButton>
+                    </Stack>
+                ) : (
+                    <Box display="flex" flexDir="column" alignItems="center">
+                        {error === AuthenticationError.NotHolder && getError()}
 
-                            {!error && <Spinner thickness="3px" emptyColor="gray.200" color="red.600" size="lg" />}
-                        </Box>
-                    )}
-                </Stack>
-            </Box>
+                        {!error && <Spinner thickness="3px" emptyColor="gray.200" color="mirage" size="lg" />}
+                    </Box>
+                )}
+            </Center>
         </Flex>
     );
 };
--- src/services/assets.tsx
+++ src/services/assets.tsx
@@ -1,5 +1,4 @@
-import Background from '../assets/backgrounds/1.jpg';
-import Unlock from '../assets/backgrounds/2.jpg';
+import Background from '../assets/backgrounds/background.png';
 import Ticket from '../assets/ticket_small.png';
 import TicketSFT from '../assets/ticket_sft.jpg';
 import Logo from '../assets/logo_small.png';
@@ -7,8 +6,7 @@ import ArtDrop from '../assets/images/Aetheris.jpg';
 
 export const getArtDrop = () => ArtDrop;
 
-export const getLayoutBackground = () => Background;
-export const getUnlockBackground = () => Unlock;
+export const getBackground = () => Background;
 
 export const getFullTicket = () => Ticket;
 export const getTicketSFT = () => TicketSFT;
